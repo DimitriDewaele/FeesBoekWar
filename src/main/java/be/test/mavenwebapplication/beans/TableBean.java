5513a9b605a145a6215d86ab084c426cbe6d5a51
Dimitri Dewaele <dimitri.dewaele@mips.be>
2015-02-13 15:23:38 +0100

    Table demo

---
+++ src/main/java/be/test/mavenwebapplication/beans/TableBean.java
@@ -0,0 +1,81 @@
+package be.test.mavenwebapplication.beans;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author dimitridw
+ */
+@Named(value = "tableBean")
+@RequestScoped
+public class TableBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private List<PersonVO> persons;
+
+    /**
+     * Creates a new instance of TableBean
+     */
+    public TableBean() {
+    }
+
+    @PostConstruct
+    private void createTable() {
+        persons = new ArrayList<PersonVO>();
+        persons.add(new PersonVO("Jean", "Pol", "receptionist", new Date(74, 10, 10)));
+        persons.add(new PersonVO("Jef", "Snijders", "guardener", new Date(71, 10, 10)));
+        persons.add(new PersonVO("Piet", "Peters", "truck driver", new Date(64, 10, 10)));
+        persons.add(new PersonVO("Peet", "Polders", "professor", new Date(54, 10, 10)));
+        persons.add(new PersonVO("Nell", "Van Den Walle", "student", new Date(12, 10, 10)));
+        persons.add(new PersonVO("Nady", "Van Toren", "cleaning", new Date(98, 10, 10)));
+        persons.add(new PersonVO("Jenny", "De Koster", "police", new Date(45, 10, 10)));
+        persons.add(new PersonVO("Malena", "Zetterman", "air traffic controler", new Date(71, 10, 10)));
+        persons.add(new PersonVO("Medina", "Zegers", "test engineer", new Date(85, 10, 10)));
+        persons.add(new PersonVO("Horaire", "Safrina", "manager", new Date(47, 10, 10)));
+
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(10);
+        for (int j = 0; j < randomInt; j++) {
+            persons.add(new PersonVO("Horaire", "Safrina", "manager", new Date(47, 10, 10)));    
+        }
+    }
+
+    /**
+     * @return the persons
+     */
+    public List<PersonVO> getPersons() {
+        return persons;
+    }
+
+    /**
+     * @param persons the persons to set
+     */
+    public void setPersons(List<PersonVO> persons) {
+        this.persons = persons;
+    }
+
+    public String getCalculatedRowClasses() {
+        StringBuffer build = new StringBuffer();
+
+        int total = persons.size();
+        build.append("list-row-0");
+
+        int factor = 99 / total;
+
+        for (int i = 0; i < total; i++) {
+            build.append(",");
+            build.append("list-row-" + (i * factor));
+        }
+
+        return build.toString();
+    }
+
+}
