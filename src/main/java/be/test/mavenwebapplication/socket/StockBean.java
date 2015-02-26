56b2dba375cacf39d61aedcb291bacb9629eb7b2
Dimitri Dewaele <dimitri.dewaele@mips.be>
2015-02-26 12:41:19 +0100

    validation

--- src/main/java/be/test/mavenwebapplication/socket/StockBean.java
+++ src/main/java/be/test/mavenwebapplication/socket/StockBean.java
@@ -1,18 +1,22 @@
 package be.test.mavenwebapplication.socket;
 
+import java.util.Date;
 import java.util.Random;
 import javax.annotation.Resource;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.ejb.TimerService;
 import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
 import javax.ejb.Timeout;
+import javax.ejb.Timer;
 import javax.ejb.TimerConfig;
 
 /**
  *
  * @author dimitridw
  */
+
 @Startup
 @Singleton
 public class StockBean {
@@ -21,7 +25,7 @@ public class StockBean {
 
     /* Use the container's timer service */
     @Resource
-    TimerService tservice;
+    private TimerService tservice;
     private Random random;
     private volatile double price = 100.0;
     private volatile int volume = 3000;
@@ -30,18 +34,24 @@ public class StockBean {
      * Creates a new instance of StockBean
      */
     public StockBean() {
+        LOGGER.info("StockBean: constructor");
     }
 
     @PostConstruct
     public void init() {
         /* Initialize the EJB and create a timer */
-        LOGGER.info("Initializing EJB.");
+        LOGGER.info("StockBean: PostConstruct");
         random = new Random();
-        tservice.createIntervalTimer(1000, 1000, new TimerConfig());
+        
+        TimerConfig timerConfig = new TimerConfig();
+        timerConfig.setInfo("StockBean timer");
+        timerConfig.setPersistent(false);
+        tservice.createIntervalTimer(10000, 10000, timerConfig);
     }
 
     @Timeout
-    public void timeout() {
+    public void timeout(Timer timer) {
+        LOGGER.info("StockBean: timeout: {}", new Date());
         /* Adjust price and volume and send updates */
         price += 1.0 * (random.nextInt(100) - 50) / 100.0;
         volume += random.nextInt(5000) - 2500;
