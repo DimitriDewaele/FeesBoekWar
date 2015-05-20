29593283dc6baa3c220dd63f5fb9a8acd03fe036
Dimitri Dewaele <dimitri.dewaele@mips.be>
2015-05-20 11:54:15 +0200

    update on application scoped bean

--- src/main/java/be/feesboek/managedbeans/ApplicationBean.java
+++ src/main/java/be/feesboek/managedbeans/ApplicationBean.java
@@ -2,6 +2,7 @@ package be.feesboek.managedbeans;
 
 import java.util.Date;
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.inject.Named;
 import javax.enterprise.context.ApplicationScoped;
 
@@ -13,6 +14,8 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class ApplicationBean {
     
+    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(ApplicationBean.class);
+    
     private Date start;
     private int count;
 
@@ -23,9 +26,15 @@ public class ApplicationBean {
     }
     
     @PostConstruct
-    public void startup() {
+    public void postConstruct() {
+        LOGGER.debug("ApplicationBean: PostConstruct");
         start = new Date();
         count = 1;
+    }
+    
+    @PreDestroy
+    public void preDestroy() {
+        LOGGER.debug("ApplicationBean: PreDestroy");
     }
     
     public int counter() {
