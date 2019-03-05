f13d560bbc1c5b333ca7af4c672400cb3eaab56a
dimitri.dewaele <dimitri.dewaele@wolterskluwer.com>
2019-03-05 11:17:30 +0100

    WildFly setup

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
-# MavenWebApplication
-Maven Web Application from NetBeans
+# FeesBoek
+FeesBoek test application for Java EE7 with maven, hibernate.
 
-Test project for Java EE7 with maven, hibernate.
+* First started as a NetBeans project.
+* Later evolved to an IntelliJ project.
+
+## Create Database
+First create the database:
+
+* src/main/resources/sql/createFeesboek-MSSQL.sql
+* src/main/resources/sql/createFeesboek-MySQL.sql
+* src/main/resources/sql/createFeesboek-Oracle.sql
+
+Make sure you can acces the DB with a user/password.
+
+## Define JNDI data source
+
+Create the JNDI data source in the application server.
+
+Reference >> Hibernate >> persistence.xml
+
+`<jta-data-source>java:/jboss/datasources/FeesboekDS</jta-data-source>`
+
+jta-data-source = The JNDI name of where the javax.sql.DataSource is located. When running without a JNDI available Datasource, you must specify JDBC connections with Hibernate specific properties (see below)
+
+## Application Servers
+Application Context:
+[http://localhost:8080/FeesBoekWar/](http://localhost:8080/FeesBoekWar/)
+
+###WildFly
+Add datasource: standalone.xml (or standalone-full.xml)
+Make sure:
+* The DB is created on MSSQL
+* Username + password is correct
+* DatabaseName is set
+* SQL driver is available on wildfly
+
+Configuration:
+
+    <datasource jndi-name="java:/jboss/datasources/FeesboekDS" pool-name="be.feesboek.PU">
+        <connection-url>jdbc:sqlserver://localhost:1433;databaseName=feesboekDB</connection-url>
+        <driver>sqlserver</driver>
+        <security>
+            <user-name>sa</user-name>
+            <password>!234Hjkl</password>
+        </security>
+        <validation>
+            <valid-connection-checker class-name="org.jboss.jca.adapters.jdbc.extensions.mssql.MSSQLValidConnectionChecker"/>
+        </validation>
+    </datasource>
+
+### GlassFish
+
+Define the JNDI datasource in the admin console.
+
+## Run the application
+				
+### GlassFish Server
+
+* Public: localhost:8080
+* Admin Console: localhost:4848
+
+## Remarks:
+
+there is also a problem with the JMS messages.
+
+It seems to run from NetBeans, but not from IntelliJ
+Some classes are put into comment, because of deployment problems:
+`be.feesboek.webmassage`
--- src/main/java/be/feesboek/webmessage/PrintManagedBean.java
+++ src/main/java/be/feesboek/webmessage/PrintManagedBean.java
@@ -1,153 +1,153 @@
-package be.feesboek.webmessage;
-
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import javax.annotation.Resource;
-import javax.inject.Named;
-import javax.enterprise.context.Dependent;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.inject.Inject;
-import javax.jms.JMSConsumer;
-import javax.jms.JMSContext;
-import javax.jms.JMSDestinationDefinition;
-import javax.jms.Message;
-import javax.jms.Queue;
-import javax.jms.QueueBrowser;
-
-@JMSDestinationDefinition(
-        name = "java:comp/jms/printQueue",
-        interfaceName = "javax.jms.Queue",
-        destinationName = "PhysicalPrintQueue")
-@Named(value = "printManagedBean")
-@Dependent
-public class PrintManagedBean {
-
-    static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(PrintManagedBean.class);
-
-    @Inject
-    private JMSContext context;
-    @Resource(lookup = "java:comp/jms/printQueue")
-    private Queue queue;
-
-    /**
-     * Creates a new instance of PrintManagedBean
-     */
-    public PrintManagedBean() {
-    }
-
-    public void sendMessage(String printer) {
-        try {
-            String text = "Message for printer: " + printer;
-
-            Message message = context.createTextMessage(text);
-            message.setStringProperty("Printer", printer);
-
-            context.createProducer().send(queue, message);
-
-            LOGGER.debug("Sending message: " + text);
-            FacesMessage facesMessage = new FacesMessage("Sending message: " + text);
-            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
-        } catch (Exception ex) {
-            LOGGER.error("SenderBean.sendMessage: Exception: {}", ex.toString());
-        }
-    }
-
-    public void getMessage(String printer) {
-        try {
-            String messageSelector = "Printer = '" + printer + "'";
-            JMSConsumer receiver = context.createConsumer(queue, messageSelector);
-            //String text = receiver.receiveBody(String.class, 1000);
-            String text = receiver.receiveBodyNoWait(String.class);
-
-            if (text != null) {
-                LOGGER.debug("Reading message: " + text);
-                FacesMessage facesMessage = new FacesMessage("Reading message: " + text);
-                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
-            } else {
-                LOGGER.debug("No message received after 1 second");
-                FacesMessage facesMessage = new FacesMessage("No message received after 1 second");
-                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
-            }
-        } catch (Exception ex) {
-            LOGGER.error("SenderBean.sendMessage: Exception: {}", ex.toString());
-        }
-    }
-
-    public void getMessageById(String id) {
-        try {
-            String messageSelector = "JMSMessageID = '" + id + "'";
-            JMSConsumer receiver = context.createConsumer(queue, messageSelector);
-            //String text = receiver.receiveBody(String.class, 1000);
-            String text = receiver.receiveBodyNoWait(String.class);
-
-            if (text != null) {
-                LOGGER.debug("Reading message: " + text);
-                FacesMessage facesMessage = new FacesMessage("Reading message: " + text);
-                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
-            } else {
-                LOGGER.debug("No message received after 1 second");
-                FacesMessage facesMessage = new FacesMessage("No message received after 1 second");
-                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
-            }
-        } catch (Exception ex) {
-            LOGGER.error("SenderBean.sendMessage: Exception: {}", ex.toString());
-        }
-    }
-
-    public void getLongMessage() {
-        try {
-            JMSConsumer receiver = context.createConsumer(queue);
-            String text = receiver.receiveBody(String.class, 10000);
-
-            if (text != null) {
-                LOGGER.debug("Reading message: " + text);
-                FacesMessage facesMessage = new FacesMessage("Reading message: " + text);
-                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
-            } else {
-                LOGGER.debug("No message received after 5 second");
-                FacesMessage facesMessage = new FacesMessage("No message received after 5 second");
-                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
-            }
-        } catch (Exception ex) {
-            LOGGER.error("SenderBean.sendMessage: Exception: {}", ex.toString());
-        }
-    }
-
-    public List<MessageVO> getList() {
-        List<MessageVO> messageList = new ArrayList<>();
-
-        try {
-            LOGGER.debug("Browse queue");
-            QueueBrowser browser = context.createBrowser(queue);
-            Enumeration msgs = browser.getEnumeration();
-
-            if (!msgs.hasMoreElements()) {
-                MessageVO empty = new MessageVO();
-                empty.setId("");
-                empty.setBody("No messages in queue");
-                empty.setProperty("");
-            } else {
-                while (msgs.hasMoreElements()) {
-                    Message temp = (Message) msgs.nextElement();
-
-                    String id = temp.getJMSMessageID();
-                    String body = temp.getBody(String.class);
-                    String prop = temp.getStringProperty("Printer");
-
-                    MessageVO messageVO = new MessageVO();
-                    messageVO.setId(id);
-                    messageVO.setBody(body);
-                    messageVO.setProperty(prop);
-
-                    messageList.add(messageVO);
-                }
-            }
-        } catch (Exception ex) {
-            LOGGER.error("SenderBean.sendMessage: Exception: {}", ex.toString());
-        }
-
-        return messageList;
-    }
-}
+//package be.feesboek.webmessage;
+//
+//import java.util.ArrayList;
+//import java.util.Enumeration;
+//import java.util.List;
+//import javax.annotation.Resource;
+//import javax.inject.Named;
+//import javax.enterprise.context.Dependent;
+//import javax.faces.application.FacesMessage;
+//import javax.faces.context.FacesContext;
+//import javax.inject.Inject;
+//import javax.jms.JMSConsumer;
+//import javax.jms.JMSContext;
+//import javax.jms.JMSDestinationDefinition;
+//import javax.jms.Message;
+//import javax.jms.Queue;
+//import javax.jms.QueueBrowser;
+//
+//@JMSDestinationDefinition(
+//        name = "java:comp/jms/printQueue",
+//        interfaceName = "javax.jms.Queue",
+//        destinationName = "PhysicalPrintQueue")
+//@Named(value = "printManagedBean")
+//@Dependent
+//public class PrintManagedBean {
+//
+//    static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(PrintManagedBean.class);
+//
+//    @Inject
+//    private JMSContext context;
+//    @Resource(lookup = "java:comp/jms/printQueue")
+//    private Queue queue;
+//
+//    /**
+//     * Creates a new instance of PrintManagedBean
+//     */
+//    public PrintManagedBean() {
+//    }
+//
+//    public void sendMessage(String printer) {
+//        try {
+//            String text = "Message for printer: " + printer;
+//
+//            Message message = context.createTextMessage(text);
+//            message.setStringProperty("Printer", printer);
+//
+//            context.createProducer().send(queue, message);
+//
+//            LOGGER.debug("Sending message: " + text);
+//            FacesMessage facesMessage = new FacesMessage("Sending message: " + text);
+//            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+//        } catch (Exception ex) {
+//            LOGGER.error("SenderBean.sendMessage: Exception: {}", ex.toString());
+//        }
+//    }
+//
+//    public void getMessage(String printer) {
+//        try {
+//            String messageSelector = "Printer = '" + printer + "'";
+//            JMSConsumer receiver = context.createConsumer(queue, messageSelector);
+//            //String text = receiver.receiveBody(String.class, 1000);
+//            String text = receiver.receiveBodyNoWait(String.class);
+//
+//            if (text != null) {
+//                LOGGER.debug("Reading message: " + text);
+//                FacesMessage facesMessage = new FacesMessage("Reading message: " + text);
+//                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+//            } else {
+//                LOGGER.debug("No message received after 1 second");
+//                FacesMessage facesMessage = new FacesMessage("No message received after 1 second");
+//                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+//            }
+//        } catch (Exception ex) {
+//            LOGGER.error("SenderBean.sendMessage: Exception: {}", ex.toString());
+//        }
+//    }
+//
+//    public void getMessageById(String id) {
+//        try {
+//            String messageSelector = "JMSMessageID = '" + id + "'";
+//            JMSConsumer receiver = context.createConsumer(queue, messageSelector);
+//            //String text = receiver.receiveBody(String.class, 1000);
+//            String text = receiver.receiveBodyNoWait(String.class);
+//
+//            if (text != null) {
+//                LOGGER.debug("Reading message: " + text);
+//                FacesMessage facesMessage = new FacesMessage("Reading message: " + text);
+//                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+//            } else {
+//                LOGGER.debug("No message received after 1 second");
+//                FacesMessage facesMessage = new FacesMessage("No message received after 1 second");
+//                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+//            }
+//        } catch (Exception ex) {
+//            LOGGER.error("SenderBean.sendMessage: Exception: {}", ex.toString());
+//        }
+//    }
+//
+//    public void getLongMessage() {
+//        try {
+//            JMSConsumer receiver = context.createConsumer(queue);
+//            String text = receiver.receiveBody(String.class, 10000);
+//
+//            if (text != null) {
+//                LOGGER.debug("Reading message: " + text);
+//                FacesMessage facesMessage = new FacesMessage("Reading message: " + text);
+//                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+//            } else {
+//                LOGGER.debug("No message received after 5 second");
+//                FacesMessage facesMessage = new FacesMessage("No message received after 5 second");
+//                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+//            }
+//        } catch (Exception ex) {
+//            LOGGER.error("SenderBean.sendMessage: Exception: {}", ex.toString());
+//        }
+//    }
+//
+//    public List<MessageVO> getList() {
+//        List<MessageVO> messageList = new ArrayList<>();
+//
+//        try {
+//            LOGGER.debug("Browse queue");
+//            QueueBrowser browser = context.createBrowser(queue);
+//            Enumeration msgs = browser.getEnumeration();
+//
+//            if (!msgs.hasMoreElements()) {
+//                MessageVO empty = new MessageVO();
+//                empty.setId("");
+//                empty.setBody("No messages in queue");
+//                empty.setProperty("");
+//            } else {
+//                while (msgs.hasMoreElements()) {
+//                    Message temp = (Message) msgs.nextElement();
+//
+//                    String id = temp.getJMSMessageID();
+//                    String body = temp.getBody(String.class);
+//                    String prop = temp.getStringProperty("Printer");
+//
+//                    MessageVO messageVO = new MessageVO();
+//                    messageVO.setId(id);
+//                    messageVO.setBody(body);
+//                    messageVO.setProperty(prop);
+//
+//                    messageList.add(messageVO);
+//                }
+//            }
+//        } catch (Exception ex) {
+//            LOGGER.error("SenderBean.sendMessage: Exception: {}", ex.toString());
+//        }
+//
+//        return messageList;
+//    }
+//}
